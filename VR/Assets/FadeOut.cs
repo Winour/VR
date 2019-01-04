c10b597ca2b47089d5e732d433281f9529333fa8
DESKTOP-0U8FLL6\usuario <danirll96@gmail.com>
2019-01-04 18:08:05 +0100

    Add menu

---
+++ VR/Assets/FadeOut.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeOut : MonoBehaviour {
+
+    public Image black;
+    private float ammount = -0.2f;
+    private Color initialColor;
+
+    private void Start()
+    {
+        initialColor = black.color;
+    }
+
+    void Update () 
+	{
+		if(ammount > 1.0f)
+        {
+            Destroy(this.gameObject);
+        }
+        ammount += Time.deltaTime * 0.4f;
+        black.color = new Color(initialColor.r, initialColor.g, initialColor.b, 1 - ammount);
+	}
+}
---
+++ VR/Assets/PlayTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayTrigger : MonoBehaviour, VRInteractive
+{
+    public void OnClickEnter()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
